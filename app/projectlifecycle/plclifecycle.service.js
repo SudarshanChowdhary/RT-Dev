19e1596a762b12bbb52702d36052cd5661074018
SudarshanChowdhary <sudarshan.ui@gmail.com>
2017-09-18 19:42:56 +0530

    worked rt_spoc service update

--- app/projectlifecycle/plclifecycle.service.js
+++ app/projectlifecycle/plclifecycle.service.js
@@ -1,33 +1,77 @@
-ProjectLifeCycleService.$inject = ['$http'];
+ProjectLifeCycleService.$inject = ['$http', '$q', 'spinnerService'];
 
-function ProjectLifeCycleService($http){
-	var rtplcmilestoneservice = {
-		rtPlcMilestoneAdd : rtPlcMilestoneAdd,
-		getRTSpocs: getRTSpocs,
-        getBhuid:getBhuid
-	};
-    return rtplcmilestoneservice;
+function ProjectLifeCycleService($http, $q, spinnerService){
+        var rtplcmilestoneservice = {
+            rtPlcMilestoneAdd : rtPlcMilestoneAdd,
+            getRTSpocs: getRTSpocs,
+            getBhuid:getBhuid
+        };
+        return rtplcmilestoneservice;
 
     function rtPlcMilestoneAdd(reqData){
-			return $http({
+        var def = $q.defer();
+        spinnerService.show();
+		$http({
             url:"milestone/add",
             data: reqData,
             method:"POST"
+        }).success(function(data) {
+            def.resolve(data);
+        })
+        .error(function() {
+            def.reject("Failed to get data");
         });
+        return def.promise;
     }
-		function getRTSpocs(){
-			return $http({
-            url:"getspocs",
-            method:"GET"
-        });
-		}
 
-        function getBhuid(id){
-            return $http({
-            url:"milestone/status/" + id,
-            method:"GET"
-        });
-        }
+    function getRTSpocs(){
+        var def = $q.defer();
+        spinnerService.show();
+        var spoc = [{name:"Manohar", email:"manohar@apple.com"},
+        {name:"Amudha", email:"Amudha@apple.com"},
+        {name:"Nithiya", email:"Nithiya@apple.com"},
+        {name:"Siva", email:"Siva@apple.com"},
+        {name:"Madhavan", email:"Madhavan@apple.com"},
+        {name:"Ravi", email:"Ravi@apple.com"},
+        {name:"Swami", email:"Swami@apple.com"},
+        {name:"Padhmi", email:"Padhmi@apple.com"},
+        {name:"Kavya", email:"Kavya@apple.com"},
+        {name:"Sudarshan", email:"Sudarshan.koyalkar@wipro.com"}];
+        def.resolve(spoc);
+
+        //*******************we need to ask swami for this service
+		// $http({
+        //     url:"milestone/getspocs",
+        //     method:"GET"
+        // }).success(function(data) {
+        //     def.resolve(data);
+        // })
+        // .error(function() {
+        //     def.reject("Failed to get data");
+        // });
+
+        return def.promise;
+	}
+
+
+    function getBhuid(id){
+        debugger;
+        var def = $q.defer();
+        spinnerService.show();
+        def.resolve("17819");
+        //*******************we need to ask swami for this service
+        // $http({
+        //     url:"milestone/status/" + id,
+        //     method:"GET"
+        // }).success(function(data) {
+        //     def.resolve(data);
+        // })
+        // .error(function() {
+        //     def.reject("Failed to get data");
+        // });
+        
+        return def.promise;
+    }
 }
 
 module.exports = ProjectLifeCycleService;
